bcc56277cced4904b9506aa217ec827a999ca722
palamapalli premsagar reddy <premsagarreddypn@gmail.com>
2020-12-30 18:05:28 +0530

    outputs

---
+++ outputs.py
@@ -0,0 +1,13 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\prems\Desktop\prem\freshworks\maincode.py ========
+key: one created successfully
+key: two created successfully
+key: three created successfully
+reading of key: two is succesfull
+error: given key: four , does not exit in database.Please enter a valid key
+error: this key already exist in the database
+key is successfully deleted
+error: given key: one , does not exit in database.Please enter a valid key
+>>> 
